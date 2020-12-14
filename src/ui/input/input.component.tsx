8c38183652dc160db5e77d394bba322a25bbd4ea
Giedrius Blažulionis <giedrius.bla@gmail.com>
2020-12-14 08:36:43 +0200

    input error

--- src/ui/input/input.component.tsx
+++ src/ui/input/input.component.tsx
@@ -4,43 +4,55 @@ import { FC, ChangeEvent, AllHTMLAttributes } from 'react';
 import styles from './input.module.scss';
 
 interface Props {
-  id: string;
+  name: string;
   type?: 'text' | 'textarea' | 'email' | 'number' | 'date' | 'file';
   inputmode?: JSX.IntrinsicElements['input']['inputMode'];
   disabled?: boolean;
   readonly?: boolean;
-  value: string;
-  onChange: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  value?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  error?: string;
+  info?: string;
+  label?: string;
+  placeholder?: string;
 }
 
 const Input: FC<Props> = ({
-  id,
+  name,
   type = 'text',
   inputmode = 'text',
   disabled,
   readonly,
   value,
   onChange,
+  error,
+  info,
+  label,
+  placeholder,
 }) => {
   const attributes: AllHTMLAttributes<
     HTMLInputElement | HTMLTextAreaElement
   > = {
-    id,
-    readOnly: readonly,
+    name,
+    placeholder,
     disabled,
+    readOnly: readonly,
     inputMode: inputmode,
     value,
     onChange,
+    'aria-describedby': 'info',
+    'aria-invalid': !!error,
   };
 
   return (
     <div className={styles.input}>
-      <label htmlFor={id}>Text Input</label>
+      {label && <label htmlFor={name}>{label}</label>}
       {type === 'textarea' ? (
         <textarea {...attributes} />
       ) : (
         <input type={type} {...attributes} />
       )}
+      {(error || info) && <small id="info">{error || info}</small>}
     </div>
   );
 };
