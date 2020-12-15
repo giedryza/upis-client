ab602c83f93582807cf704e0cb4f40fca6021927
Giedrius Blažulionis <giedrius.bla@gmail.com>
2020-12-15 20:06:10 +0200

    add axe

---
+++ src/utils/dev-tools/axe.ts
@@ -0,0 +1,25 @@
+import { isServer } from 'utils/common/is-server';
+
+export class Axe {
+  get isAvailable() {
+    return !isServer() && process.env.NODE_ENV === 'development';
+  }
+
+  private start = async () => {
+    const React = (await import('react')).default;
+    const ReactDOM = (await import('react-dom')).default;
+    const axeCore = (await import('@axe-core/react')).default;
+    const debounce = 1000;
+    const config = {};
+
+    axeCore(React, ReactDOM, debounce, config);
+  };
+
+  init = () => {
+    if (this.isAvailable) {
+      this.start();
+    }
+  };
+}
+
+export const axe = new Axe();
