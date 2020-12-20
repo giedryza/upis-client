98117f8622d6f167f8e5862ec39b6def8b705d1c
Giedrius Blažulionis <giedrius.bla@gmail.com>
2020-12-20 16:10:44 +0200

    app layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,16 @@ import { AppProps } from 'next/app';
 import 'styles/index.scss';
 
 import { axe } from 'utils/dev-tools/axe';
+import { AppLayout } from 'layouts/app/app.layout';
 
 axe.init();
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
-import { Header } from 'components/header/header.component';
-
 const Home = () => {
   return (
     <div>
-      <Header />
       <h1>next</h1>
     </div>
   );
