1e999a146d25d7cce1287a3ab72bc5ac0b3972f7
Giedrius Blažulionis <giedrius.bla@gmail.com>
2020-12-25 11:14:31 +0200

    initial fetch setup

---
+++ src/utils/lib/http/http.lib.ts
@@ -0,0 +1,34 @@
+import { baseUrl } from './http.constants';
+
+interface Config {
+  method?: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+  headers?: Record<string, string>;
+  body?: Record<string, any>;
+}
+
+export const http = async (endpoint: string, config: Config = {}) => {
+  const { method = 'GET', headers = {}, body = null } = config;
+
+  const defaults = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const init: RequestInit = {
+    method,
+    headers: new Headers({
+      ...defaults.headers,
+      ...headers,
+    }),
+    body: body ? JSON.stringify(body) : undefined,
+  };
+
+  const response = await fetch(`${baseUrl}/${endpoint}`, init);
+
+  const json = await response.json();
+
+  if (!response.ok) return Promise.reject(json);
+
+  return json;
+};
