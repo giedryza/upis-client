fd07450e4d7b9f88b695ce6374b6bda265fdeea0
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-01-25 19:38:25 +0200

    unused constant

--- src/ui/file-input/file-input.constants.ts
+++ src/ui/file-input/file-input.constants.ts
@@ -1,12 +1,1 @@
-import { DragState } from './file-input.types';
-
-import { IconName } from 'ui/icon/icon.component';
-
 export const FILES_LIMIT = 5;
-
-export const ICON_BY_DRAG_STATE: Record<DragState, IconName> = {
-  active: IconName.CirclePlus,
-  inactive: IconName.CirclePlus,
-  accept: IconName.CircleCheck,
-  reject: IconName.CircleX,
-};
