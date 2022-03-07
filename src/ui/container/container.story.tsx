8a1933a4528156f67183ef07d9ac166df7efcf79
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-03-07 14:20:08 +0200

    add story: container

---
+++ src/ui/container/container.story.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Container } from '.';
+
+export default {
+  title: 'UI/Container',
+  component: Container,
+  args: {},
+} as ComponentMeta<typeof Container>;
+
+const Template: ComponentStory<typeof Container> = (args) => (
+  <Container {...args}>
+    <div style={{ backgroundColor: 'deepskyblue', height: 200 }} />
+  </Container>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  ...Default.args,
+};
