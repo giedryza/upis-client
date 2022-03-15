e34dc8422f7a386ad4ecf717c77eb6163689cafb
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-03-15 20:50:30 +0200

    cleanup

--- src/components/account/companies/company-create/company-create.constants.ts
+++
@@ -1,8 +0,0 @@
-import { CompanyCreateFormValues } from 'components/account/company/company-create/company-create.types';
-
-export const INITIAL_VALUES: CompanyCreateFormValues = {
-  name: '',
-  phone: '',
-  email: '',
-  description: '',
-};
--- src/components/account/companies/company-create/company-create.types.ts
+++
@@ -1,6 +0,0 @@
-export type CompanyCreateFormValues = {
-  name: string;
-  phone: string;
-  email: string;
-  description: string;
-};
