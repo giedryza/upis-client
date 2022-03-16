fcfafae6afbdcbdd8aa33e53796ddbc5ae62dfb9
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-03-16 19:27:19 +0200

    group icons

--- src/ui/icon/icon.story.tsx
+++ src/ui/icon/icon.story.tsx
@@ -10,32 +10,128 @@ export default {
 } as ComponentMeta<typeof Icon>;
 
 const Template: ComponentStory<typeof Icon> = (args) => (
-  <div style={{ display: 'flex', gap: 20, flexWrap: 'wrap' }}>
-    {Object.values(IconName).map((name) => (
-      <div key={name}>
-        <div
-          style={{
-            backgroundColor: '#f1f2f4',
-            padding: 15,
-            width: 60,
-            height: 60,
-            margin: 'auto',
-          }}
-        >
-          <Icon {...args} name={name} />
-        </div>
-        <span
-          style={{
-            display: 'inline-block',
-            width: 100,
-            textAlign: 'center',
-            padding: 5,
-          }}
-        >
-          {name}
-        </span>
-      </div>
-    ))}
+  <div style={{ display: 'flex', flexDirection: 'column', gap: 50 }}>
+    <div style={{ display: 'flex', gap: 20, flexWrap: 'wrap' }}>
+      {Object.values(IconName)
+        .filter(
+          (name) =>
+            !name.startsWith('logo-') &&
+            !name.startsWith('flag-') &&
+            !name.startsWith('util-')
+        )
+        .map((name) => (
+          <div key={name}>
+            <div
+              style={{
+                backgroundColor: '#f1f2f4',
+                padding: 15,
+                width: 60,
+                height: 60,
+                margin: 'auto',
+              }}
+            >
+              <Icon {...args} name={name} />
+            </div>
+            <span
+              style={{
+                display: 'inline-block',
+                width: 100,
+                textAlign: 'center',
+                padding: 5,
+              }}
+            >
+              {name}
+            </span>
+          </div>
+        ))}
+    </div>
+    <div style={{ display: 'flex', gap: 20, flexWrap: 'wrap' }}>
+      {Object.values(IconName)
+        .filter((name) => name.startsWith('logo-'))
+        .map((name) => (
+          <div key={name}>
+            <div
+              style={{
+                backgroundColor: '#f1f2f4',
+                padding: 15,
+                width: 60,
+                height: 60,
+                margin: 'auto',
+              }}
+            >
+              <Icon {...args} name={name} />
+            </div>
+            <span
+              style={{
+                display: 'inline-block',
+                width: 100,
+                textAlign: 'center',
+                padding: 5,
+              }}
+            >
+              {name}
+            </span>
+          </div>
+        ))}
+    </div>
+    <div style={{ display: 'flex', gap: 20, flexWrap: 'wrap' }}>
+      {Object.values(IconName)
+        .filter((name) => name.startsWith('flag-'))
+        .map((name) => (
+          <div key={name}>
+            <div
+              style={{
+                backgroundColor: '#f1f2f4',
+                padding: 15,
+                width: 60,
+                height: 60,
+                margin: 'auto',
+              }}
+            >
+              <Icon {...args} name={name} />
+            </div>
+            <span
+              style={{
+                display: 'inline-block',
+                width: 100,
+                textAlign: 'center',
+                padding: 5,
+              }}
+            >
+              {name}
+            </span>
+          </div>
+        ))}
+    </div>
+    <div style={{ display: 'flex', gap: 20, flexWrap: 'wrap' }}>
+      {Object.values(IconName)
+        .filter((name) => name.startsWith('util-'))
+        .map((name) => (
+          <div key={name}>
+            <div
+              style={{
+                backgroundColor: '#f1f2f4',
+                padding: 15,
+                width: 60,
+                height: 60,
+                margin: 'auto',
+              }}
+            >
+              <Icon {...args} name={name} />
+            </div>
+            <span
+              style={{
+                display: 'inline-block',
+                width: 100,
+                textAlign: 'center',
+                padding: 5,
+              }}
+            >
+              {name}
+            </span>
+          </div>
+        ))}
+    </div>
   </div>
 );
 
