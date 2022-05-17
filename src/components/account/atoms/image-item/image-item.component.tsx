b236827cac22873edda57e9ed54eaee0c68521ac
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-05-17 20:52:03 +0300

    use image placeholder

--- src/components/account/atoms/image-item/image-item.component.tsx
+++ src/components/account/atoms/image-item/image-item.component.tsx
@@ -1,16 +1,18 @@
 import { VFC } from 'react';
 
+import { ImagePlaceholder } from 'ui';
+
 import { Props } from './image-item.types';
 import styles from './image-item.module.scss';
 
 export const ImageItem: VFC<Props> = ({ src }) => {
   return (
     <div className={styles.content}>
-      <img
-        src={src || '/images/placeholder.png'}
-        className={styles.image}
-        alt="logo"
-      />
+      {src ? (
+        <img src={src} className={styles.image} alt="logo" />
+      ) : (
+        <ImagePlaceholder width={375} />
+      )}
     </div>
   );
 };
