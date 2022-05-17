d0950921060d8c859a9147b8ba2d4acf9f497221
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-05-17 20:51:49 +0300

    ui: image-placeholder, add width prop

--- src/ui/image-placeholder/image-placeholder.component.tsx
+++ src/ui/image-placeholder/image-placeholder.component.tsx
@@ -5,9 +5,9 @@ import { Icon } from 'ui/icon';
 import { Props } from './image-placeholder.types';
 import styles from './image-placeholder.module.scss';
 
-export const ImagePlaceholder: VFC<Props> = () => {
+export const ImagePlaceholder: VFC<Props> = ({ width }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ maxWidth: width }}>
       <Icon name="picture" className={styles.icon} />
     </div>
   );
