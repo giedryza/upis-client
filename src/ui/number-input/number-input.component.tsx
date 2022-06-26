572ebe7c78b275cdcb7ca9e36ffe86c297fa7ddb
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-06-26 20:34:49 +0300

    forward ref

--- src/ui/number-input/number-input.component.tsx
+++ src/ui/number-input/number-input.component.tsx
@@ -1,112 +1,120 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { useRef, VFC } from 'react';
+import { forwardRef, useRef } from 'react';
 import { useButton, useLocale, useNumberField } from 'react-aria';
 import { useNumberFieldState } from 'react-stately';
+import { useObjectRef } from '@react-aria/utils';
 
 import { Props } from './number-input.types';
 import styles from './number-input.module.scss';
 
-export const NumberInput: VFC<Props> = ({
-  label,
-  placeholder,
-  value,
-  onChange,
-  disabled,
-  readonly,
-  min,
-  max,
-  step,
-  error,
-  info,
-  autofocus,
-  stepper = false,
-  formatOptions,
-}) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const incrementRef = useRef<HTMLButtonElement>(null);
-  const decrementRef = useRef<HTMLButtonElement>(null);
+export const NumberInput = forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      label,
+      placeholder,
+      value,
+      onChange,
+      disabled,
+      readonly,
+      min,
+      max,
+      step,
+      error,
+      info,
+      autofocus,
+      stepper = false,
+      formatOptions,
+    },
+    forwardedRef
+  ) => {
+    const incrementRef = useRef<HTMLButtonElement>(null);
+    const decrementRef = useRef<HTMLButtonElement>(null);
 
-  const { locale } = useLocale();
+    const inputRef = useObjectRef(forwardedRef);
+    const { locale } = useLocale();
 
-  const numberFieldProps: Parameters<typeof useNumberField>[0] = {
-    label,
-    placeholder,
-    value,
-    onChange,
-    isDisabled: disabled,
-    isReadOnly: readonly,
-    minValue: min,
-    maxValue: max,
-    step,
-    errorMessage: error,
-    description: info,
-    validationState: error ? 'invalid' : 'valid',
-    autoFocus: autofocus,
-    formatOptions,
-  };
-  const state = useNumberFieldState({
-    ...numberFieldProps,
-    locale,
-  });
+    const numberFieldProps: Parameters<typeof useNumberField>[0] = {
+      label,
+      placeholder,
+      value,
+      onChange,
+      isDisabled: disabled,
+      isReadOnly: readonly,
+      minValue: min,
+      maxValue: max,
+      step,
+      errorMessage: error,
+      description: info,
+      validationState: error ? 'invalid' : 'valid',
+      autoFocus: autofocus,
+      formatOptions,
+    };
+    const state = useNumberFieldState({
+      ...numberFieldProps,
+      locale,
+    });
 
-  const {
-    labelProps,
-    groupProps,
-    inputProps,
-    descriptionProps,
-    errorMessageProps,
-    incrementButtonProps,
-    decrementButtonProps,
-  } = useNumberField(numberFieldProps, state, inputRef);
-  const { buttonProps: incrementProps } = useButton(
-    incrementButtonProps,
-    incrementRef
-  );
-  const { buttonProps: decrementProps } = useButton(
-    decrementButtonProps,
-    decrementRef
-  );
+    const {
+      labelProps,
+      groupProps,
+      inputProps,
+      descriptionProps,
+      errorMessageProps,
+      incrementButtonProps,
+      decrementButtonProps,
+    } = useNumberField(numberFieldProps, state, inputRef);
+    const { buttonProps: incrementProps } = useButton(
+      incrementButtonProps,
+      incrementRef
+    );
+    const { buttonProps: decrementProps } = useButton(
+      decrementButtonProps,
+      decrementRef
+    );
 
-  return (
-    <div className={styles.container}>
-      <label className={styles.label} {...labelProps}>
-        {label}
-      </label>
+    return (
+      <div className={styles.container}>
+        <label className={styles.label} {...labelProps}>
+          {label}
+        </label>
 
-      <div className={styles.inputContainer} {...groupProps}>
-        {stepper && (
-          <button
-            className={styles.stepper}
-            {...decrementProps}
-            type="button"
-            ref={decrementRef}
-          >
-            -
-          </button>
+        <div className={styles.inputContainer} {...groupProps}>
+          {stepper && (
+            <button
+              className={styles.stepper}
+              {...decrementProps}
+              type="button"
+              ref={decrementRef}
+            >
+              -
+            </button>
+          )}
+          <input className={styles.input} {...inputProps} ref={inputRef} />
+          {stepper && (
+            <button
+              className={styles.stepper}
+              {...incrementProps}
+              type="button"
+              ref={incrementRef}
+            >
+              +
+            </button>
+          )}
+        </div>
+
+        {info && (
+          <small {...descriptionProps} data-help="info">
+            {info}
+          </small>
         )}
-        <input className={styles.input} {...inputProps} ref={inputRef} />
-        {stepper && (
-          <button
-            className={styles.stepper}
-            {...incrementProps}
-            type="button"
-            ref={incrementRef}
-          >
-            +
-          </button>
+        {error && (
+          <small {...errorMessageProps} data-help="error">
+            {error}
+          </small>
         )}
       </div>
+    );
+  }
+);
 
-      {info && (
-        <small {...descriptionProps} data-help="info">
-          {info}
-        </small>
-      )}
-      {error && (
-        <small {...errorMessageProps} data-help="error">
-          {error}
-        </small>
-      )}
-    </div>
-  );
-};
+NumberInput.displayName = 'NumberInput';
