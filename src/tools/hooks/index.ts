b68cd3b04b5b94490e25e0672a739fd00eb1a451
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-07-03 19:07:52 +0300

    rename on click outside hook

--- src/tools/hooks/index.ts
+++ src/tools/hooks/index.ts
@@ -1,4 +1,4 @@
 export * from './use-guest-page';
-export * from './use-on-click-outside.hook';
+export * from './use-on-click-outside';
 export * from './use-protected-page';
 export * from './use-timeout';
