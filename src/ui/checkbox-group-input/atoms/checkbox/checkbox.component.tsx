ed0e956d789d2c00aff204245f93cda10815ffaf
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-07-24 21:00:50 +0300

    forward ref

--- src/ui/checkbox-group-input/atoms/checkbox/checkbox.component.tsx
+++ src/ui/checkbox-group-input/atoms/checkbox/checkbox.component.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { FC, useRef } from 'react';
+import { forwardRef } from 'react';
 import clsx from 'clsx';
 import {
   mergeProps,
@@ -7,53 +7,52 @@ import {
   useFocusRing,
   useVisuallyHidden,
 } from 'react-aria';
+import { useObjectRef } from '@react-aria/utils';
 
 import { useCheckboxContext } from '../../checkbox-group-input.context';
 
 import { Props } from './checkbox.types';
 import styles from './checkbox.module.scss';
 
-export const Checkbox: FC<Props> = ({
-  children,
-  value,
-  error,
-  disabled,
-  readonly,
-}) => {
-  const ref = useRef<HTMLInputElement>(null);
-  const state = useCheckboxContext();
+export const Checkbox = forwardRef<HTMLInputElement, Props>(
+  ({ children, value, error, disabled, readonly }, forwardedRef) => {
+    const ref = useObjectRef(forwardedRef);
+    const state = useCheckboxContext();
 
-  const { inputProps } = useCheckboxGroupItem(
-    {
-      children,
-      value,
-      name: value,
-      isDisabled: disabled,
-      isReadOnly: readonly,
-      validationState: error ? 'invalid' : 'valid',
-    },
-    state,
-    ref
-  );
-  const { visuallyHiddenProps } = useVisuallyHidden();
-  const { focusProps, isFocusVisible } = useFocusRing();
+    const { inputProps } = useCheckboxGroupItem(
+      {
+        children,
+        value,
+        name: value,
+        isDisabled: disabled,
+        isReadOnly: readonly,
+        validationState: error ? 'invalid' : 'valid',
+      },
+      state,
+      ref
+    );
+    const { visuallyHiddenProps } = useVisuallyHidden();
+    const { focusProps, isFocusVisible } = useFocusRing();
 
-  return (
-    <label
-      className={clsx(styles.checkbox, {
-        [`${styles.selected}`]: state.isSelected(value),
-        [`${styles.focus}`]: isFocusVisible,
-        [`${styles.disabled}`]: state.isDisabled || disabled,
-        [`${styles.readonly}`]: state.isReadOnly || readonly,
-      })}
-    >
-      <input
-        {...mergeProps(visuallyHiddenProps, inputProps, focusProps, { ref })}
-      />
+    return (
+      <label
+        className={clsx(styles.checkbox, {
+          [`${styles.selected}`]: state.isSelected(value),
+          [`${styles.focus}`]: isFocusVisible,
+          [`${styles.disabled}`]: state.isDisabled || disabled,
+          [`${styles.readonly}`]: state.isReadOnly || readonly,
+        })}
+      >
+        <input
+          {...mergeProps(visuallyHiddenProps, inputProps, focusProps, { ref })}
+        />
 
-      <div className={styles.box} />
+        <div className={styles.box} />
 
-      <span className={styles.label}>{children}</span>
-    </label>
-  );
-};
+        <span className={styles.label}>{children}</span>
+      </label>
+    );
+  }
+);
+
+Checkbox.displayName = 'Checkbox';
--- src/ui/checkbox-group-input/checkbox-group-input.component.tsx
+++ src/ui/checkbox-group-input/checkbox-group-input.component.tsx
@@ -1,31 +1,35 @@
-import { VFC } from 'react';
+import { forwardRef } from 'react';
 
 import { Props } from './checkbox-group-input.types';
 import { Checkbox, Group } from './atoms';
 
-export const CheckboxGroupInput: VFC<Props> = ({
-  label,
-  items,
-  value,
-  onChange,
-  error,
-  disabled,
-  readonly,
-}) => {
-  return (
-    <Group
-      label={label}
-      onChange={onChange}
-      value={value}
-      error={error}
-      disabled={disabled}
-      readonly={readonly}
-    >
-      {items.map((item) => (
-        <Checkbox {...item} error={!!error} key={item.value}>
-          {item.label}
-        </Checkbox>
-      ))}
-    </Group>
-  );
-};
+export const CheckboxGroupInput = forwardRef<HTMLInputElement, Props>(
+  (
+    { label, items, value, onChange, error, disabled, readonly },
+    forwardedRef
+  ) => {
+    return (
+      <Group
+        label={label}
+        onChange={onChange}
+        value={value}
+        error={error}
+        disabled={disabled}
+        readonly={readonly}
+      >
+        {items.map((item) => (
+          <Checkbox
+            {...item}
+            error={!!error}
+            ref={forwardedRef}
+            key={item.value}
+          >
+            {item.label}
+          </Checkbox>
+        ))}
+      </Group>
+    );
+  }
+);
+
+CheckboxGroupInput.displayName = 'CheckboxGroupInput';
