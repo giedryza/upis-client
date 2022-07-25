92e0b43c4fd7edbe5bfd2854c6a92d86f3803700
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-07-25 08:16:01 +0300

    create query key factory

--- src/domain/users/users.keys.ts
+++ src/domain/users/users.keys.ts
@@ -1,7 +1,3 @@
-export const usersKeys = {
-  all: ['users'] as const,
-  lists: () => [...usersKeys.all, 'list'] as const,
-  list: (...args: any[]) => [...usersKeys.lists(), ...args] as const,
-  details: () => [...usersKeys.all, 'detail'] as const,
-  detail: (id: string) => [...usersKeys.details(), id] as const,
-};
+import { keyFactory } from 'tools/services/query-client';
+
+export const usersKeys = keyFactory('users');
---
+++ src/tools/services/query-client/query-client.utils.ts
@@ -0,0 +1,13 @@
+export const keyFactory = (key: string) => {
+  const root = [key] as const;
+  const lists = [...root, 'list'] as const;
+  const details = [...root, 'detail'] as const;
+
+  return {
+    root,
+    lists,
+    list: (...deps: any[]) => [...lists, ...deps] as const,
+    details,
+    detail: (id: string, ...deps: any[]) => [...details, id, ...deps] as const,
+  };
+};
