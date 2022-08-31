fca63daf2392650badd0c527bcf7bd543c86c910
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-08-31 14:05:57 +0300

    new loaders pattern in users

--- src/domain/users/users.loaders.ts
+++ src/domain/users/users.loaders.ts
@@ -1,61 +1,69 @@
 import { endpoints } from 'config/endpoints';
-import { Request, getJsonBody } from 'tools/services/request';
+import { Request, getJsonBody, loadersFactory } from 'tools/services/request';
 
 import { Session } from './users.types';
 
-export const loaders = {
-  signup: ({
-    email,
-    password,
-    confirmPassword,
-  }: {
-    email: string;
-    password: string;
-    confirmPassword: string;
-  }) =>
-    new Request<Session>(endpoints.users.signup, {
-      body: getJsonBody({
-        email,
-        password,
-        confirmPassword,
-      }),
-    }).post(),
-  updatePassword: ({
-    currentPassword,
-    newPassword,
-    confirmPassword,
-  }: {
-    currentPassword: string;
-    newPassword: string;
-    confirmPassword: string;
-  }) =>
-    new Request(endpoints.users.password.update, {
-      body: getJsonBody({
-        currentPassword,
-        newPassword,
-        confirmPassword,
-      }),
-    }).patch(),
-  forgotPassword: ({ email }: { email: string }) =>
-    new Request(endpoints.users.password.forgot, {
-      body: getJsonBody({
-        email,
-      }),
-    }).post(),
-  resetPassword: ({
-    userId,
-    token,
-    password,
-  }: {
-    userId: string;
-    token: string;
-    password: string;
-  }) =>
-    new Request(endpoints.users.password.reset, {
-      body: getJsonBody({
-        userId,
-        token,
-        password,
-      }),
-    }).post(),
-};
+interface Signup {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+interface UpdatePassword {
+  currentPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+}
+
+interface ForgotPassword {
+  email: string;
+}
+
+interface ResetPassword {
+  userId: string;
+  token: string;
+  password: string;
+}
+
+export const { getLoaders, useLoaders } = loadersFactory((locale) => ({
+  loaders: {
+    signup: ({ email, password, confirmPassword }: Signup) =>
+      new Request<Session>(endpoints.users.signup, {
+        body: getJsonBody({
+          email,
+          password,
+          confirmPassword,
+        }),
+        locale,
+      }).post(),
+    updatePassword: ({
+      currentPassword,
+      newPassword,
+      confirmPassword,
+    }: UpdatePassword) =>
+      new Request(endpoints.users.password.update, {
+        body: getJsonBody({
+          currentPassword,
+          newPassword,
+          confirmPassword,
+        }),
+        locale,
+      }).patch(),
+    forgotPassword: ({ email }: ForgotPassword) =>
+      new Request(endpoints.users.password.forgot, {
+        body: getJsonBody({
+          email,
+        }),
+        locale,
+      }).post(),
+    resetPassword: ({ userId, token, password }: ResetPassword) =>
+      new Request(endpoints.users.password.reset, {
+        body: getJsonBody({
+          userId,
+          token,
+          password,
+        }),
+        locale,
+      }).post(),
+  },
+}));
--- src/domain/users/users.mutations.ts
+++ src/domain/users/users.mutations.ts
@@ -1,9 +1,11 @@
 import { useMutation } from 'react-query';
 import { signIn } from 'next-auth/react';
 
-import { loaders } from './users.loaders';
+import { useLoaders } from './users.loaders';
 
 export const useSignup = () => {
+  const { loaders } = useLoaders();
+
   const mutation = useMutation(loaders.signup, {
     onSuccess: (_data, { email, password }) => {
       signIn<'credentials'>('credentials', {
@@ -18,18 +20,24 @@ export const useSignup = () => {
 };
 
 export const useUpdatePassword = () => {
+  const { loaders } = useLoaders();
+
   const mutation = useMutation(loaders.updatePassword);
 
   return mutation;
 };
 
 export const useForgotPassword = () => {
+  const { loaders } = useLoaders();
+
   const mutation = useMutation(loaders.forgotPassword);
 
   return mutation;
 };
 
 export const useResetPassword = () => {
+  const { loaders } = useLoaders();
+
   const mutation = useMutation(loaders.resetPassword);
 
   return mutation;
