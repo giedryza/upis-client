b8c8d0283e2a9c4c32b0a387ee8877fb0547ff41
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-09-20 06:23:25 +0300

    tour amenities display

---
+++ src/components/account/tour/atoms/amenities/amenities.component.tsx
@@ -0,0 +1,54 @@
+import { VFC } from 'react';
+import useTranslation from 'next-translate/useTranslation';
+
+import { InfoBlock } from 'components/account/atoms';
+import { useActiveTour } from 'domain/tours';
+import { routes } from 'config/routes';
+import { EmptyState, Tile } from 'ui';
+import { ICON_BY_VARIANT } from 'domain/amenities';
+import { formatCurrency } from 'tools/format';
+
+export const Amenities: VFC = () => {
+  const { t, lang } = useTranslation();
+
+  const { data: tour } = useActiveTour();
+
+  if (!tour) return null;
+
+  return (
+    <InfoBlock
+      title={t('account:tours.amenities.title')}
+      icon="link"
+      columns={2}
+      editPage={routes.account.tours.one.about.replace(':id', tour._id)}
+    >
+      {tour.amenities.length ? (
+        tour.amenities.map((amenity) => (
+          <Tile
+            title={t(`common:amenities.variants.${amenity.variant}`)}
+            subtitle={amenity.info}
+            icon={ICON_BY_VARIANT[amenity.variant]}
+            fields={[
+              {
+                label: t('account:companies.amenities.form.amount.display'),
+                sublabel: amenity.price
+                  ? `${formatCurrency(
+                      lang,
+                      amenity.price.amount,
+                      amenity.price.currency
+                    )} ${t(`common:amenities.units.${amenity.unit}`)}`
+                  : t('common:texts.free'),
+              },
+            ]}
+            key={amenity._id}
+          />
+        ))
+      ) : (
+        <EmptyState
+          title={t('account:tours.amenities.texts.empty.title')}
+          message={t('account:tours.amenities.texts.empty.message')}
+        />
+      )}
+    </InfoBlock>
+  );
+};
