4058b324bdfa5a46fdf94922a4fd32a8250ad65d
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-10-03 10:33:57 +0300

    refactor map item

--- src/components/account/atoms/map-item/map-item.component.tsx
+++ src/components/account/atoms/map-item/map-item.component.tsx
@@ -5,14 +5,23 @@ import { Map, mapIcon } from 'ui';
 import { Props } from './map-item.types';
 import styles from './map-item.module.scss';
 
-export const MapItem: VFC<Props> = ({ lat, lng }) => {
-  if (!lat || !lng) return null;
+export const MapItem: VFC<Props> = ({ markers }) => {
+  if (
+    markers
+      .map((marker) => [marker.lat, marker.lng])
+      .flat()
+      .filter((point) => point === 0).length
+  ) {
+    return null;
+  }
 
   return (
     <div className={styles.container}>
       <Map
-        center={{ lat, lng }}
-        zoom={16}
+        bounds={markers.map((marker) => [marker.lat, marker.lng])}
+        boundsOptions={{
+          padding: [20, 20],
+        }}
         zoomControl={false}
         boxZoom={false}
         dragging={false}
@@ -21,12 +30,21 @@ export const MapItem: VFC<Props> = ({ lat, lng }) => {
         tap={false}
         touchZoom={false}
       >
-        {({ leaflet: { icon }, reactLeaflet: { Marker } }) => (
-          <Marker
-            position={{ lat, lng }}
-            icon={icon(mapIcon({ name: 'pin', size: 32 }))}
-          />
-        )}
+        {({ leaflet: { icon }, reactLeaflet: { Marker, Tooltip } }) =>
+          markers.map((marker, i) => (
+            <Marker
+              position={{ lat: marker.lat, lng: marker.lng }}
+              icon={icon(mapIcon({ name: 'pin', size: 32 }))}
+              key={i}
+            >
+              {!!marker.label && (
+                <Tooltip direction="right" permanent offset={[10, -20]}>
+                  {marker.label}
+                </Tooltip>
+              )}
+            </Marker>
+          ))
+        }
       </Map>
     </div>
   );
