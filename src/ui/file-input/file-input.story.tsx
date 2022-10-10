29af28ddcf5cc564546d82ed98086ead09bc65e8
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-10-10 14:58:58 +0300

    lint fix

--- src/ui/file-input/file-input.story.tsx
+++ src/ui/file-input/file-input.story.tsx
@@ -22,15 +22,3 @@ export const Default = Template.bind({});
 Default.args = {
   ...Default.args,
 };
-
-export const Multiple = Template.bind({});
-Multiple.args = {
-  ...Default.args,
-  maxFiles: 5,
-};
-
-export const Disabled = Template.bind({});
-Disabled.args = {
-  ...Default.args,
-  disabled: true,
-};
