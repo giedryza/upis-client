ee96f3630ad3f15d8c1a32fe627816c25cb7488d
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-10-11 08:56:28 +0300

    gallery preview

--- src/components/account/tour/atoms/gallery/gallery.component.tsx
+++ src/components/account/tour/atoms/gallery/gallery.component.tsx
@@ -1,14 +1,24 @@
 import { FC } from 'react';
+import { useQueryClient } from 'react-query';
 import useTranslation from 'next-translate/useTranslation';
 
-import { useActiveTour } from 'domain/tours';
+import { ImageBox } from 'ui';
 import { InfoBlock } from 'components/account/atoms';
+import { toursKeys, useActiveTour } from 'domain/tours';
 import { routes } from 'config/routes';
+import { useDeleteImage } from 'domain/images';
+import { useConfirm } from 'domain/confirm';
+
+import styles from './gallery.module.scss';
 
 export const Gallery: FC = () => {
   const { t } = useTranslation();
+  const queryClient = useQueryClient();
+
+  const { confirmation } = useConfirm();
 
   const { data: tour } = useActiveTour();
+  const { mutate: deleteImage, isLoading } = useDeleteImage();
 
   if (!tour) return null;
 
@@ -20,13 +30,48 @@ export const Gallery: FC = () => {
       actions={[
         {
           url: routes.account.tours.one.gallery.replace(':id', tour._id),
-          label: t('common:actions.edit'),
+          label: t('common:actions.add'),
           variant: 'tertiary',
-          icon: 'pencil',
+          icon: 'plus',
         },
       ]}
     >
-      {null}
+      <div className={styles.gallery}>
+        {tour.photos.map((photo, i) => (
+          <ImageBox
+            image={photo.url}
+            alt={photo.description}
+            actions={[
+              {
+                icon: 'trash',
+                attributes: {
+                  title: t('common:actions.delete'),
+                  onClick: async () => {
+                    const { confirmed } = await confirmation(
+                      t('account:tours.gallery.texts.confirmDelete')
+                    );
+
+                    if (confirmed) {
+                      deleteImage(
+                        { id: photo._id },
+                        {
+                          onSuccess: () => {
+                            queryClient.invalidateQueries(
+                              toursKeys.detail(tour._id)
+                            );
+                          },
+                        }
+                      );
+                    }
+                  },
+                  disabled: isLoading,
+                },
+              },
+            ]}
+            key={i}
+          />
+        ))}
+      </div>
     </InfoBlock>
   );
 };
