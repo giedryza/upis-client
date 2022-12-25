c696bf9c5a22a79f25322d500dff88628931810b
Giedrius Blažulionis <giedrius.bla@gmail.com>
2022-12-25 20:34:55 +0200

    add fluid fit to image placeholder

--- src/ui/image-placeholder/image-placeholder.component.tsx
+++ src/ui/image-placeholder/image-placeholder.component.tsx
@@ -5,9 +5,12 @@ import { Icon } from 'ui/icon';
 import { Props } from './image-placeholder.types';
 import styles from './image-placeholder.module.scss';
 
-export const ImagePlaceholder: FC<Props> = ({ width }) => {
+export const ImagePlaceholder: FC<Props> = ({ fit = 'fixed' }) => {
   return (
-    <div className={styles.container} style={{ maxWidth: width }}>
+    <div
+      className={styles.container}
+      style={fit === 'fluid' ? { width: '100%', height: '100%' } : {}}
+    >
       <Icon name="picture" className={styles.icon} />
     </div>
   );
