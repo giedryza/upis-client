7f46fddcd20a3a97df7cd3cb201ee552e9c7dae2
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-01-03 19:12:32 +0200

    manually apply regions filter

--- src/components/serp/filters/atoms/regions/regions.component.tsx
+++ src/components/serp/filters/atoms/regions/regions.component.tsx
@@ -1,9 +1,12 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { useRouter } from 'next/router';
+import { useForm, Controller } from 'react-hook-form';
 import useTranslation from 'next-translate/useTranslation';
 
 import { Region, regions, useToursFilters } from 'domain/tours';
 import { CheckboxGroupInput, Pill } from 'ui';
+
+import { Values } from './regions.types';
 
 export const FilterRegions: FC = () => {
   const { t } = useTranslation();
@@ -11,28 +14,44 @@ export const FilterRegions: FC = () => {
 
   const { data: filters } = useToursFilters();
 
+  const values = useMemo<Values>(
+    () => ({ regions: filters?.regions ?? [] }),
+    [filters?.regions]
+  );
+  const isEmpty = !values.regions.length;
+
   const navigateWithFilters = (value: Region[]) => {
     push({ query: { ...query, regions: value } }, undefined, {
       shallow: true,
     });
   };
 
+  const {
+    control,
+    getValues,
+    formState: { isDirty },
+  } = useForm<Values>({ values, shouldUnregister: true });
+
   return (
     <Pill
       label={t('serp:filters.regions.title')}
       title={t('serp:filters.regions.title')}
-      active={Boolean(filters?.regions?.length)}
+      active={!isEmpty}
       popover={
-        <CheckboxGroupInput
-          ariaLabel={t('serp:filters.regions.title')}
-          items={regions.map((region) => ({
-            label: t(`regions:${region}`),
-            value: region,
-          }))}
-          value={filters?.regions ?? []}
-          onChange={(value) => {
-            navigateWithFilters(value as Region[]);
-          }}
+        <Controller
+          control={control}
+          name="regions"
+          render={({ field: { onChange, value } }) => (
+            <CheckboxGroupInput
+              ariaLabel={t('serp:filters.regions.title')}
+              items={regions.map((region) => ({
+                label: t(`regions:${region}`),
+                value: region,
+              }))}
+              value={value}
+              onChange={onChange}
+            />
+          )}
         />
       }
       actions={[
@@ -43,6 +62,18 @@ export const FilterRegions: FC = () => {
             onClick: () => {
               navigateWithFilters([]);
             },
+            disabled: isEmpty,
+          },
+          closable: true,
+        },
+        {
+          label: t('common:actions.apply'),
+          variant: 'primary',
+          attributes: {
+            onClick: () => {
+              navigateWithFilters(getValues().regions);
+            },
+            disabled: !isDirty,
           },
           closable: true,
         },
