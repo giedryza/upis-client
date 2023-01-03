587b352f9e6dd95a40b3203177f44557f0b373d6
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-01-03 19:32:08 +0200

    manually apply rivers filter

--- src/components/serp/filters/atoms/rivers/rivers.component.tsx
+++ src/components/serp/filters/atoms/rivers/rivers.component.tsx
@@ -1,9 +1,12 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { useRouter } from 'next/router';
+import { useForm, Controller } from 'react-hook-form';
 import useTranslation from 'next-translate/useTranslation';
 
 import { River, rivers, useToursFilters } from 'domain/tours';
 import { MultiAutocompleteInput, Pill } from 'ui';
+
+import { Values } from './rivers.types';
 
 export const FilterRivers: FC = () => {
   const { t } = useTranslation();
@@ -11,33 +14,49 @@ export const FilterRivers: FC = () => {
 
   const { data: filters } = useToursFilters();
 
+  const values = useMemo<Values>(
+    () => ({ rivers: filters?.rivers ?? [] }),
+    [filters?.rivers]
+  );
+  const isEmpty = !values.rivers.length;
+
   const navigateWithFilters = (value: River[]) => {
     push({ query: { ...query, rivers: value } }, undefined, {
       shallow: true,
     });
   };
 
+  const {
+    control,
+    getValues,
+    formState: { isDirty },
+  } = useForm<Values>({ values, shouldUnregister: true });
+
   return (
     <Pill
       label={t('serp:filters.rivers.title')}
       title={t('serp:filters.rivers.title')}
-      active={Boolean(filters?.rivers?.length)}
+      active={!isEmpty}
       popover={
-        <MultiAutocompleteInput
+        <Controller
+          control={control}
           name="rivers"
-          ariaLabel={t('serp:filters.rivers.title')}
-          placeholder={t('common:actions.search')}
-          items={
-            rivers.map((river) => ({
-              label: t(`rivers:${river}`),
-              value: river,
-            })) ?? []
-          }
-          value={filters?.rivers ?? []}
-          onChange={(value) => {
-            navigateWithFilters(value as River[]);
-          }}
-          autofocus
+          render={({ field: { onChange, value } }) => (
+            <MultiAutocompleteInput
+              name="rivers"
+              ariaLabel={t('serp:filters.rivers.title')}
+              placeholder={t('common:actions.search')}
+              items={
+                rivers.map((river) => ({
+                  label: t(`rivers:${river}`),
+                  value: river,
+                })) ?? []
+              }
+              value={value}
+              onChange={onChange}
+              autofocus
+            />
+          )}
         />
       }
       actions={[
@@ -48,6 +67,18 @@ export const FilterRivers: FC = () => {
             onClick: () => {
               navigateWithFilters([]);
             },
+            disabled: isEmpty,
+          },
+          closable: true,
+        },
+        {
+          label: t('common:actions.apply'),
+          variant: 'primary',
+          attributes: {
+            onClick: () => {
+              navigateWithFilters(getValues().rivers);
+            },
+            disabled: !isDirty,
           },
           closable: true,
         },
