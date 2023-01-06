53763f4e3d9dcc6ccbce347caca088104ebf5c60
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-01-06 14:59:53 +0200

    scrollable filters bar

--- src/components/serp/filters/filters.component.tsx
+++ src/components/serp/filters/filters.component.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { clsx } from 'clsx';
 import useTranslation from 'next-translate/useTranslation';
 
 import { APP } from 'config/app';
@@ -17,35 +18,29 @@ import styles from './filters.module.scss';
 export const SerpFilters: FC = () => {
   const { t } = useTranslation();
 
+  const items = [
+    <FilterAmenities key="amenities" />,
+    <FilterRegions key="regions" />,
+    <FilterRivers key="rivers" />,
+    <FilterDays key="days" />,
+    <FilterDuration key="duration" />,
+    <FilterDistance key="distance" />,
+    <FilterDifficulty key="difficulty" />,
+  ] as const;
+
   return (
     <nav
       className={styles.nav}
       style={{ '--spacing': APP.serp.gridGap }}
       aria-label={t('serp:filters.title')}
     >
-      <ul className={styles.list}>
-        <li>
-          <FilterAmenities />
-        </li>
-        <li>
-          <FilterRegions />
-        </li>
-        <li>
-          <FilterRivers />
-        </li>
-        <li>
-          <FilterDays />
-        </li>
-        <li>
-          <FilterDuration />
-        </li>
-        <li>
-          <FilterDistance />
-        </li>
-        <li>
-          <FilterDifficulty />
-        </li>
-      </ul>
+      <div className={clsx(styles.swiper, 'scrollbar-hidden')}>
+        <ul className={styles.list}>
+          {items.map((item, i) => (
+            <li key={i}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
