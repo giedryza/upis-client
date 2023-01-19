5fbdb10c8d220e3a427620ac320a60276ada6348
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-01-19 21:06:08 +0200

    parse env variables with zod

--- src/types/global/env.d.ts
+++ src/types/global/env.d.ts
@@ -1,7 +1,20 @@
-import { Env } from 'types/common';
+import { z } from 'zod';
+
+const nodeEnv = ['production', 'development', 'test'] as const;
+
+const envVariables = z.object({
+  NODE_ENV: z.enum(nodeEnv),
+  NEXT_PUBLIC_HOST_CLIENT: z.string(),
+  NEXT_PUBLIC_HOST_API: z.string(),
+  NEXTAUTH_URL: z.string(),
+  NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_JWT_EXPIRES_IN_DAYS: z.string(),
+});
+
+envVariables.parse(process.env);
 
 declare global {
   namespace NodeJS {
-    interface ProcessEnv extends Env {}
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
   }
 }
