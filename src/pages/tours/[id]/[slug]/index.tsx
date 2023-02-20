49c57b037189acad1b92fa5aaa40b1bbb873e329
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-02-20 16:03:10 +0200

    load tour on server

--- src/pages/tours/[id]/[slug]/index.tsx
+++ src/pages/tours/[id]/[slug]/index.tsx
@@ -1,14 +1,19 @@
 import { GetServerSideProps, NextPage } from 'next';
 import { getSession } from 'next-auth/react';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
 
 import { AppHead } from 'ui';
 import { TourDetails } from 'components/tour';
 import { MainLayout } from 'layouts';
+import { getRouteParam } from 'tools/common';
+import { getLoaders, toursKeys, useActiveTour } from 'domain/tours';
 
 const TourPage: NextPage = () => {
+  const { data: tour } = useActiveTour();
+
   return (
     <>
-      <AppHead />
+      <AppHead title={tour?.name} />
 
       <MainLayout>
         <TourDetails />
@@ -17,12 +22,25 @@ const TourPage: NextPage = () => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+  locale,
+}) => {
   const session = await getSession({ req });
+
+  const queryClient = new QueryClient();
+  const id = getRouteParam(params?.id);
+  const { loaders } = getLoaders(locale);
+
+  await queryClient.prefetchQuery(toursKeys.detail(id), () =>
+    loaders.getTour({ req, id })
+  );
 
   return {
     props: {
       session,
+      dehydratedState: dehydrate(queryClient),
     },
   };
 };
