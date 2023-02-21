9d584634456f419ab105cf4f23d9727b2ceca006
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-02-21 10:38:40 +0200

    details layout

--- src/components/tour/details/details.component.tsx
+++ src/components/tour/details/details.component.tsx
@@ -1,15 +1,21 @@
 import { FC } from 'react';
 
 import { Container } from 'ui';
+import { APP } from 'config';
 
-import { DetailsHeader } from './atoms';
+import { DetailsBody, DetailsHeader, DetailsSidebar } from './atoms';
 import styles from './details.module.scss';
 
 export const TourDetails: FC = () => {
   return (
     <Container>
-      <main className={styles.main}>
+      <main className={styles.main} style={{ '--spacing': APP.serp.gridGap }}>
         <DetailsHeader />
+
+        <div className={styles.sections}>
+          <DetailsBody />
+          <DetailsSidebar />
+        </div>
       </main>
     </Container>
   );
