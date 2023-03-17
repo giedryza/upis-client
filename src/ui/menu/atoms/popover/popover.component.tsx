75ad1ee15c415ac53226cfeda2dc4cc3dcec52d9
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-03-17 13:19:23 +0200

    lint fix

--- src/ui/menu/atoms/popover/popover.component.tsx
+++ src/ui/menu/atoms/popover/popover.component.tsx
@@ -2,6 +2,7 @@ import { FC, PropsWithChildren, useRef } from 'react';
 import { DismissButton, Overlay, usePopover } from 'react-aria';
 
 import { Props } from './popover.types';
+import styles from './popover.module.scss';
 
 export const Popover: FC<PropsWithChildren<Props>> = ({
   children,
@@ -16,8 +17,8 @@ export const Popover: FC<PropsWithChildren<Props>> = ({
 
   return (
     <Overlay>
-      <div {...underlayProps} style={{ position: 'fixed', inset: 0 }} />
-      <div {...popoverProps} ref={ref} style={popoverProps.style}>
+      <div {...underlayProps} className={styles.underlay} />
+      <div {...popoverProps} style={popoverProps.style} ref={ref}>
         <DismissButton onDismiss={state.close} />
         {children}
         <DismissButton onDismiss={state.close} />
