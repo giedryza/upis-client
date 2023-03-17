aeb85f6b7cc55785c1ae39315f7269f785a43938
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-03-17 16:43:13 +0200

    update account menu

--- src/components/layout/header/atoms/account-menu/account-menu.component.tsx
+++ src/components/layout/header/atoms/account-menu/account-menu.component.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react';
 import useTranslation from 'next-translate/useTranslation';
+import { useRouter } from 'next/router';
 import { useSession, signOut } from 'next-auth/react';
 
 import { routes } from 'config';
-import { Button, DropdownMenu } from 'ui';
+import { Button, Menu } from 'ui';
 import { generateUrl } from 'tools/services';
 
 export const AccountMenu: FC = () => {
   const { t } = useTranslation();
+  const { push } = useRouter();
 
   const { status } = useSession();
 
@@ -16,31 +18,47 @@ export const AccountMenu: FC = () => {
   };
 
   return status === 'authenticated' ? (
-    <DropdownMenu
-      id="account-menu"
-      position="bottom-right"
-      menuButton={{
-        as: 'button',
-        label: t('common:account.account'),
-        icon: 'user',
-        variant: 'ghost',
-        size: 'sm',
-        withDropdown: true,
-      }}
-      items={[
+    <Menu
+      ariaLabel={t('common:account.account')}
+      sections={[
         {
-          as: 'link',
-          label: t('common:account.profile'),
-          icon: 'user',
-          href: generateUrl(routes.account.profile.index),
+          id: 'account',
+          label: t('common:account.account'),
+          items: [
+            {
+              id: 'profile',
+              label: t('common:account.profile'),
+              icon: 'user',
+              onClick: () => push(generateUrl(routes.account.profile.index)),
+            },
+            {
+              id: 'providers',
+              label: t('common:account.providers'),
+              icon: 'kayak',
+              onClick: () => push(generateUrl(routes.account.providers.index)),
+            },
+            {
+              id: 'tours',
+              label: t('common:account.tours'),
+              icon: 'path',
+              onClick: () => push(generateUrl(routes.account.tours.index)),
+            },
+          ],
         },
         {
-          as: 'button',
-          label: t('common:account.signout'),
-          icon: 'exit',
-          onClick: signout,
+          id: 'signout',
+          items: [
+            {
+              id: 'signout',
+              label: t('common:account.signout'),
+              icon: 'exit',
+              onClick: signout,
+            },
+          ],
         },
       ]}
+      icon="user"
+      size="sm"
     />
   ) : (
     <Button
