2f7ff94ec76a31d90f0f07d6850067d1c1c91695
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-04-03 14:14:11 +0300

    update middleware config

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,15 @@ import { NextRequest, NextResponse } from 'next/server';
 import { routes } from 'config';
 import { redirect } from 'tools/services';
 
-export const config: Record<'matcher', Array<typeof routes.redirect>> = {
+export const config: Record<'matcher', Array<typeof routes.redirect>> &
+  Record<string, any> = {
   matcher: ['/redirect'],
+  unstable_allowDynamic: [
+    '/node_modules/axe-core/axe.js',
+    '/node_modules/@babel/runtime/regenerator/index.js',
+    '/node_modules/@cloudinary/transformation-builder-sdk/internal/utils/cloneDeep.js',
+    '/node_modules/@cloudinary/url-gen/internal/utils/cloneDeep.js',
+  ],
 };
 
 export const middleware = async ({ nextUrl }: NextRequest) => {
