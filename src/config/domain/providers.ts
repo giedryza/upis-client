6d4b1140d39b901a1ae29a1d87e70ee085f991c3
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-04-10 13:29:58 +0300

    add tiktok

--- src/config/domain/providers.ts
+++ src/config/domain/providers.ts
@@ -1,5 +1,12 @@
 export const PROVIDERS = {
   boats: ['single-kayak', 'double-kayak', 'triple-kayak', 'raft'],
-  socials: ['facebook', 'instagram', 'youtube', 'linkedin', 'twitter'],
+  socials: [
+    'facebook',
+    'instagram',
+    'youtube',
+    'linkedin',
+    'tiktok',
+    'twitter',
+  ],
   select: ['_id', 'name'],
 } as const;
