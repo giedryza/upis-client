eebd3ad75f7577e1bf464e914932b48e4e1e1a2d
Giedrius Blažulionis <giedrius.bla@gmail.com>
2023-05-31 10:36:28 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,11 +31,12 @@ export const getServerSideProps: GetServerSideProps = async ({
   try {
     const { loaders } = getLoaders(locale);
     // const filters = getQueryParams(routes.home, query);
+    const filters = {};
 
     await Promise.all([
-      // queryClient.prefetchInfiniteQuery(toursKeys.list(filters), () =>
-      //   loaders.getTours({ params: filters })
-      // ),
+      queryClient.prefetchInfiniteQuery(toursKeys.list(filters), () =>
+        loaders.getTours({ params: filters })
+      ),
       queryClient.prefetchQuery(toursKeys.list('filters', 'summary'), () =>
         loaders.getFiltersSummary()
       ),
